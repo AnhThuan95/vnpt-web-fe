c2cfcee531950ca9ec1a39d11eabe3e76025aee5
thuan <thuan.hust.95@gmail.com>
2020-07-03 17:36:15 +0700

    duplicate code

--- src/app/news/vnpt-news/create/create.component.ts
+++ src/app/news/vnpt-news/create/create.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup} from "@angular/forms";
+import {NewsService} from "../../../service/news.service";
+import {Router} from "@angular/router";
+import {UploadService} from "../../../service/upload.service";
+import {HttpErrorResponse} from "@angular/common/http";
 
 @Component({
   selector: 'app-create',
@@ -6,10 +11,78 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./create.component.css']
 })
 export class CreateComponent implements OnInit {
+  data: FormGroup;
+  url: any;
 
-  constructor() { }
+  selectedFile: File;
+  message: string;
 
-  ngOnInit(): void {
+  constructor(private fb: FormBuilder, private newsService: NewsService,
+              private router: Router, private uploadService: UploadService) {
   }
 
+  ngOnInit() {
+    this.data = this.fb.group({
+      title: '',
+      imgUrl: {
+        name: '',
+      },
+      description: '',
+      content: '',
+      author: '',
+      category: 'vnpt'
+    });
+  }
+
+  public onFileChanged(event) {
+    this.selectedFile = event.target.files[0];
+  }
+
+  onUpload() {
+    console.log(this.selectedFile);
+
+    const uploadImageData = new FormData();
+    uploadImageData.append('imageFile', this.selectedFile, this.selectedFile.name);
+
+    this.uploadService.uploadImage(uploadImageData).subscribe(next => {
+        console.log(next);
+      }, (error: HttpErrorResponse) => {
+        console.log(error);
+        if (error.status === 200) {
+          this.url = error.error.text;
+          this.message = 'Image uploaded successfully';
+        } else {
+          this.message = 'Image not uploaded successfully';
+        }
+      }
+    );
+  }
+
+  submit() {
+    if (this.url === undefined) {
+      this.url = '';
+    }
+    this.data.value.imgUrl.name = this.url;
+    console.log(this.data.value);
+
+    this.newsService.createNews(this.data.value).subscribe(next => {
+        console.log(next);
+        alert('Created news!');
+      }, (error: HttpErrorResponse) => {
+        console.log(error);
+        if (error.status === 200) {
+          alert(error.error.text);
+          this.router.navigate(['news/vnpt-news']).then(e => {
+            if (e) {
+              console.log('Navigation is successful!');
+            } else {
+              console.log('Navigation has failed!');
+            }
+          });
+        } else {
+          alert('Có lỗi xảy ra, tạo tin không thành công!');
+        }
+      }
+    );
+  }
 }
--- src/app/news/vnpt-news/vnpt-news.component.ts
+++ src/app/news/vnpt-news/vnpt-news.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {INews} from "../../interface/i-news";
+import {NewsService} from "../../service/news.service";
 
 @Component({
   selector: 'app-vnpt-news',
@@ -6,10 +8,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./vnpt-news.component.css']
 })
 export class VnptNewsComponent implements OnInit {
+  news: INews[];
+  message: string;
+  news3: INews[];
 
-  constructor() { }
+  pageOfItems: Array<any>;
+  size = 5;
 
-  ngOnInit(): void {
+  constructor(private newsService: NewsService) { }
+
+  ngOnInit(){
+    this.newsService.getVnptNews().subscribe(next => {
+      this.news = next;
+      console.log(next);
+    }, error => {
+      this.message = error.error;
+    });
+
+    this.newsService.get3VnptNews().subscribe(next => {
+      this.news3 = next;
+    })
   }
 
+  onChangePage(pageOfItems: Array<any>) {
+    this.pageOfItems = pageOfItems;
+  }
 }
